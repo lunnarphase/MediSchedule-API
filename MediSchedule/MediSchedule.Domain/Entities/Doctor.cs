4a0cbd614d7ab8fadb88bdb856798307603c68b4
Grzegorz Kotkowski <149706238+lunnarphase@users.noreply.github.com>
2026-01-07 18:37:01 +0100

    Add Doctor and Patient entities
    Introduced Doctor and Patient entity classes in the Domain layer.

---
+++ MediSchedule/MediSchedule.Domain/Entities/Doctor.cs
@@ -0,0 +1,16 @@
+﻿namespace MediSchedule.Domain.Entities
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Specialization { get; set; } = default!;
+
+        // Stawka godzinowa / za wizytę
+        public decimal BaseRate { get; set; }
+
+        // Soft delete - zamiast usuwać z bazy, będziemy deaktywować
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ MediSchedule/MediSchedule.Domain/Entities/Patient.cs
@@ -0,0 +1,15 @@
+﻿namespace MediSchedule.Domain.Entities
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+
+        // PESEL jako string, ponieważ może zaczynać się od zera oraz nie wykonujemy na nim działań matematycznych
+        public string Pesel { get; set; } = default!;
+
+        public bool IsActive { get; set; } = true;
+    }
+}
