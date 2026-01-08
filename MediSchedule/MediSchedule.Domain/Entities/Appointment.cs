a68ef3725995d27ca0d14c17f6ccb0ad0d05c5a8
Grzegorz Kotkowski <149706238+lunnarphase@users.noreply.github.com>
2026-01-08 16:17:11 +0100

    Add Appointment entity and AppointmentStatus enum
    Introduced the Appointment entity with properties for scheduling, pricing, and relationships to Doctor and Patient. Added the AppointmentStatus enum to represent the status of an appointment.

---
+++ MediSchedule/MediSchedule.Domain/Entities/Appointment.cs
@@ -0,0 +1,20 @@
+﻿using MediSchedule.Domain.Enums;
+
+namespace MediSchedule.Domain.Entities
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; } // Data i godzina rozpoczęcia wizyty
+        public int DurationMinutes { get; set; } // Czas trwania w minutach (np. 15, 30, 60)
+        public decimal Price { get; set; } // Cena wizyty (wyliczana w momencie rezerwacji)
+        public AppointmentStatus Status { get; set; } = AppointmentStatus.Scheduled; // Domyślny status przy utworzeniu to 'Scheduled'
+
+
+        // ---------- RELACJE (Foreign Keys) -----------
+        public int DoctorId { get; set; } // Klucz obcy do lekarza
+        public Doctor Doctor { get; set; } = default!; // Właściwość nawigacyjna (pozwala pobrać dane lekarza z obiektu wizyty)
+        public int PatientId { get; set; } // Klucz obcy do pacjenta
+        public Patient Patient { get; set; } = default!;  // Właściwość nawigacyjna (pozwala pobrać dane pacjenta z obiektu wizyty)
+    }
+}
---
+++ MediSchedule/MediSchedule.Domain/Enums/AppointmentStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace MediSchedule.Domain.Enums
+{
+    public enum AppointmentStatus
+    {
+        Scheduled = 1, // Zaplanowana
+        Completed = 2, // Zrealizowana
+        Canceled = 3   // Anulowana
+    }
+}
