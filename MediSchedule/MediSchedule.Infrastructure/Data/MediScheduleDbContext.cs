9f6c02d0da5d33c14c0d57a720989398af2d3c32
Grzegorz Kotkowski <149706238+lunnarphase@users.noreply.github.com>
2026-01-08 18:31:46 +0100

    Add MediScheduleDbContext with entity configurations
    Introduces the MediScheduleDbContext class for Entity Framework Core, including DbSet properties for Doctor, Patient, and Appointment entities.

---
+++ MediSchedule/MediSchedule.Infrastructure/Data/MediScheduleDbContext.cs
@@ -0,0 +1,28 @@
+﻿using MediSchedule.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediSchedule.Infrastructure.Data
+{
+    public class MediScheduleDbContext : DbContext
+    {
+        // Konstruktor przyjmujący opcje (np. connection string)
+        public MediScheduleDbContext(DbContextOptions<MediScheduleDbContext> options) : base(options)
+        {
+        }
+
+        // Reprezentacja tabel w bazie danych
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Konfiguracja precyzji dla ceny poniewaz domyślnie EF może nie ustawić odpowiedniego typu w bazie danych
+            modelBuilder.Entity<Appointment>()
+                .Property(a => a.Price)
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
